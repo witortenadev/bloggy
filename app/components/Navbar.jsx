047284c2a61e6b550f4e2be95e324bc81e13e902
witortenadev <witortena@edu.unifil.br>
2025-02-19 17:30:54 -0300

    Add files via upload

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import { MdClose, MdMenu } from "react-icons/md";
+
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <nav
+      className={`flex flex-col sm:flex-row w-full ${
+        isMenuOpen ? "items-center" : "items-end px-4"
+      } justify-center bg-slate-800 py-2`}
+    >
+      <button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="translate-y-1 flex sm:hidden mb-2"
+      >
+        {isMenuOpen ? <MdClose size={25} /> : <MdMenu size={25} />}
+      </button>
+      <ul
+        className={`${
+          isMenuOpen ? "flex" : "hidden"
+        } flex-col sm:flex sm:flex-row max-w-screen-xl gap-4 mx-auto font-bold text-white transition-all`}
+      >
+        {["Home", "Posts", "Users"].map((item, i) => (
+          <NavItem key={i} label={item} />
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+function NavItem({ label }) {
+  let link = null;
+  if (label == "Home") {
+    link = "/";
+  }
+  return (
+    <Link href={link ? link : label.toLowerCase()}>
+      <li className="cursor-pointer rounded-sm p-1 hover:bg-slate-500">
+        {label}
+      </li>
+    </Link>
+  );
+}
+
+export default Navbar;
